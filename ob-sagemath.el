f3ffbad674349f6356d175d95a7fa95b07d1a1ed
Sho Takemori <stakemorii@gmail.com>
2016-03-07 23:22:30 +0900

    Update sage-shell-mode version

--- ob-sagemath.el
+++ ob-sagemath.el
@@ -4,7 +4,7 @@
 
 ;; Author: Sho Takemori <stakemorii@gmail.com>
 ;; URL: https://github.com/stakemori/ob-sagemath
-;; Package-Requires: ((sage-shell-mode "0.0.8") (s "1.8.0") (emacs "24"))
+;; Package-Requires: ((sage-shell-mode "0.0.9") (s "1.8.0") (emacs "24"))
 ;; Version: 0.1
 ;; Keywords: SageMath, org-babel
 
