017c3b8599380083affccbeaef0174cc9af60e4b
Sho Takemori <stakemorii@gmail.com>
2016-04-14 10:26:54 +0900

    Add tests

--- tests/tests.el
+++ tests/tests.el
@@ -5,31 +5,48 @@
 (setq ob-sagemath-output-display-function #'ignore)
 
 (setq org-babel-default-header-args:sage '((:session . t)
-                                           (:results . "output")))
+                                           (:results . "output")
+                                           (:latexnewline . "\\\\\n")
+                                           (:latexwrap . ("\\(" . "\\)"))))
+
+(defun ob-sagemath-test-exec (params body)
+  (org-babel-execute:sage
+   body
+   (append params org-babel-default-header-args:sage)))
 
 (when (executable-find "sage")
-  (defun ob-sagemath-test-exec (params body)
-    (org-babel-execute:sage body params))
 
   (ert-deftest ob-sagemth-latex-code-block ()
-    (and (should (string= (ob-sagemath-test-exec
-                           '((:tolatex . "yes"))
-                           "sqrt(x)")
-                          "\\sqrt{x}"))
-         (should (string= (ob-sagemath-test-exec
-                           nil
-                           "show(sqrt(x))")
-                          "\\sqrt{x}"))))
+    (should (string= (ob-sagemath-test-exec
+                      '((:tolatex . "yes")
+                        (:latexwrap . nil))
+                      "sqrt(x)")
+                     "\\sqrt{x}"))
+    (should (string= (ob-sagemath-test-exec
+                      nil
+                      "show(sqrt(x))")
+                     "\\(\\sqrt{x}\\)"))
+    (should (string= (ob-sagemath-test-exec
+                      '((:tolatex . "yes")
+                        (:latexwrap . ("\\begin{gather*}\n" . "\n\\end{gather*}")))
+                      "sqrt(x)\nsqrt(x)")
+                     "\\begin{gather*}
+\\sqrt{x}\\\\
+\\sqrt{x}
+\\end{gather*}")))
+
   (ert-deftest ob-sagemath-results-value ()
     (should (string= (ob-sagemath-test-exec
                       '((:result-params "value"))
                       "print 1\n2")
                      "2")))
+
   (ert-deftest ob-sagemath-image ()
     (let ((file (make-temp-file "ob-sage" nil ".png")))
-      (ob-sagemath-test-exec
-       `((:file . ,file))
-       "plot(sin, (0, 2*pi))")
+      (should (string= (ob-sagemath-test-exec
+                        `((:file . ,file))
+                        "plot(sin, (0, 2*pi))")
+                       file)) 
       (should
        (> (f-size file) 0))
       (delete-file file))))
